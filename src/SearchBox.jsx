4a546b99457397a7844c3244bd9ea89beb57e08d
Manoj2202178 <manojkumarkolichelimi10@gmail.com>
2026-01-05 21:11:25 +0530

    change in API

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -6,7 +6,7 @@ export default function SearchBox({updateInfo}){
     let [city, setCity]=useState("");
     let [error, setError]=useState(false)
     const API_URL="https://api.openweathermap.org/data/2.5/weather"
-    const API_KEY="7daac3865c3dab46cc54afb334eb819d"
+    const API_KEY=import.meta.env.VITE_WEATHER_API_KEY
 
     let getWeatherInfo= async()=>{
         try{
